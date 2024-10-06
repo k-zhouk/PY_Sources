9f41db295ad1fe9c3bf103c72e43f4c7abe1dbfb
k-zhouk <konstantin_zhouk@outlook.com>
2024-10-06 18:57:45 +0200

    One more problems has been solved

--- Recursion_Problems/unique_paths_rec.py
+++ Recursion_Problems/unique_paths_rec.py
@@ -1,10 +1,12 @@
-def get_unique_paths(rows, columns):
-    if (rows== 0) and (columns== 0):
-        return 0
+def get_unique_paths(rows, cols):
+    if (rows== 1) or (cols== 1):
+        return 1
+    
+    return get_unique_paths(rows- 1, cols)+ get_unique_paths(rows, cols- 1)
 
 def main():
-    test_row= 3
-    test_col= 4
+    test_row= 13
+    test_col= 13
     print("\nNumber of rows: {0}\nNumber of coumns: {1}\n".format(test_row, test_col))
 
     paths= get_unique_paths(test_row, test_col)
